25f20c19f3cfed290c641b4b7f0c99cd7a30ae0a
Leonxc <llxxcc000@sina.com>
2015-01-30 14:29:35 +0800

    wrote a readme file

---
+++ GameEntity.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python
+# -*-coding: utf-8 -*-
+
+import pygame
+from gameobjects.vector2 import Vector2
+
+class GameEntity():
+	def __init__(self, name):
+		self.name = name
+		self.image = pygame.image
+		self.speed = 0.
+		self.location = Vector2(0, 0)
+		self.destination = Vector2(0, 0)
+		
+	def render(self):
+		
